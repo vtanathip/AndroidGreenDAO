a6414a6aea27e5aaa9672079da3fdd8f2c757dad
Vtanathip <tanathip.se@gmail.com>
2014-03-22 10:22:56 +0700

    use simple CRUD functions of greendao

--- app/src/main/java/com/martroutine/android/androidgreendao/app/MainActivity.java
+++ app/src/main/java/com/martroutine/android/androidgreendao/app/MainActivity.java
@@ -1,17 +1,57 @@
 package com.martroutine.android.androidgreendao.app;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.martroutine.android.greendaoexample.DaoMaster;
+import com.martroutine.android.greendaoexample.DaoSession;
+import com.martroutine.android.greendaoexample.Note;
+import com.martroutine.android.greendaoexample.NoteDao;
+
+import java.util.Date;
+import java.util.List;
+
 
 public class MainActivity extends Activity {
+
+    private final static String TAG = "ANDROIDGREENDAO";
+    private SQLiteDatabase db;
+    private DaoMaster.DevOpenHelper helper;
+    private DaoSession daoSession;
+    private DaoMaster daoMaster;
+    private NoteDao noteDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helper = new DaoMaster.DevOpenHelper(this, "notes-db", null);
+        db = helper.getWritableDatabase();
+        daoMaster = new DaoMaster(db);
+        daoSession = daoMaster.newSession();
+
+        noteDao = daoSession.getNoteDao();
+        Note note = new Note(null, "mart","testing green dao", new Date());
+        noteDao.insert(note);
+        Log.d(TAG, "Inserted new note, ID: " + note.getId());
+
+        List<Note> notes = noteDao.loadAll();
+        Log.d(TAG,"Note size: " + notes.size());
+        for (Note data : notes){
+            Log.d(TAG,"Comment from database: " + data.getComment());
+        }
+
+        List<Note> specificList = noteDao.queryBuilder().where(NoteDao.Properties.Text.eq("vtanathip")).list();
+        Log.d(TAG,"specific list size: " + specificList.size());
+        for (Note data : specificList){
+            Log.d(TAG,"specific text from database: " + data.getText());
+        }
+
     }
 
 
